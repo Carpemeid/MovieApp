b6615c37dce0c5bf44e4da63232d4fb59f8dd733
Carpemeid <andonidan@gmail.com>
2017-05-14 22:10:28 +0200

    - Added movies view controller - Added table view in storyboards - Added close button icon image

---
+++ MovieApp/ViewControllers/MovieSearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MovieSearchViewController.swift
+//  MovieApp
+//
+//  Created by Dan Andoni on 14.05.17.
+//  Copyright © 2017 Dan Andoni. All rights reserved.
+//
+
+import UIKit
+
+class MovieSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
